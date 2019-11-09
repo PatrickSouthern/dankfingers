5840ea75cedbd5ca2777aa5375135e9456b7f1bc
PatrickSouthern <patrick.j.southern@gmail.com>
2019-11-08 22:39:12 -0500

    sup

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Dankfingers, coming soon.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://dankfingers.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Dank Fingers. For the stickiest of icky.
         </a>
       </header>
     </div>
