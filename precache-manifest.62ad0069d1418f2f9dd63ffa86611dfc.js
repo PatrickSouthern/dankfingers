e182ca481c4ba49da6488f801fa15a807db93eee
PatrickSouthern <patrick.j.southern@gmail.com>
2019-11-08 23:45:52 -0500

    Updates

---
+++ precache-manifest.62ad0069d1418f2f9dd63ffa86611dfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f23271079b9ff98ee73d25c580ac676",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f76fa390ae41d4688ce2",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "41bf4191a8f6dada5148",
+    "url": "/static/js/2.38fd1e49.chunk.js"
+  },
+  {
+    "revision": "f76fa390ae41d4688ce2",
+    "url": "/static/js/main.3f526de8.chunk.js"
+  },
+  {
+    "revision": "7b3eb75917875ed1751c",
+    "url": "/static/js/runtime-main.257099e1.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
